63046946bbd4527f839aef6d065b35c0be82362c
kolec <lukasz.kolczynski@3e.pl>
2020-12-19 20:29:28 +0100

    clean up

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const listener = require('ws-listener');
+const authorization = require('./authorization');
+const orderService = require('./orders-service');
+const productService = require('./products-service');
+
+module.exports = {
+    authorization,
+    listener, 
+    orderService,
+    productService
+}
