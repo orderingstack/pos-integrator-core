a96d913024554f110c724f785b7a95288012a148
kolec <lukasz.kolczynski@3e.pl>
2021-01-03 13:14:48 +0100

    import products enhancement 2

--- src/products-service.js
+++ src/products-service.js
@@ -7,30 +7,32 @@ productsToImport: [
 ...
 ]
 */
-async function importProductsToOrderingStack (productsToImport, authDataProviderCallbackAsync, alterProductBeforeImportCallback, concurrencyLevel=10) {
+async function importProductsToOrderingStack(productsToImport, authDataProviderCallbackAsync, alterProductBeforeImportCallback, concurrencyLevel = 10) {
   //one by one approach 
   const result = [];
-  for (const prod of productsToImport) {  
-    const {access_token} = await authDataProviderCallbackAsync(); 
-    const res = await importSingleProduct(prod, access_token, alterProductBeforeImportCallback);
+  let index = 1;
+  const count = productsToImport.length;
+  for (const prod of productsToImport) {
+    const { access_token } = await authDataProviderCallbackAsync();
+    const res = await importSingleProduct(prod, access_token, alterProductBeforeImportCallback, index++, count);
     result.push(res);
   }
-/*
-  //parallel approach
-  let result = [];
-  const promises = productsToImport.map(prod=>importSingleProduct(prod, token, alterProductBeforeImportCallback));
-  while (promises.length) {
-    // concurrencyLevel at at time
-    const batchResult = await Promise.all( promises.splice(0, concurrencyLevel) );
-    await new Promise(resolve => setTimeout(resolve, 500));
-    result = result.concat(batchResult);
-  }
-  */
+  /*
+    //parallel approach
+    let result = [];
+    const promises = productsToImport.map(prod=>importSingleProduct(prod, token, alterProductBeforeImportCallback));
+    while (promises.length) {
+      // concurrencyLevel at at time
+      const batchResult = await Promise.all( promises.splice(0, concurrencyLevel) );
+      await new Promise(resolve => setTimeout(resolve, 500));
+      result = result.concat(batchResult);
+    }
+    */
   const output = {
-    imported: 0,      
+    imported: 0,
     errors: []
   }
-  result.map(e=>{if (e.ok) output.imported++; else output.errors.push(e.err)});
+  result.map(e => { if (e.ok) output.imported++; else output.errors.push(e.err) });
   return output;
 }
 
@@ -42,11 +44,11 @@ async function importProductsToOrderingStack (productsToImport, authDataProvider
  * @param {*} alterProductBeforeImportCallback - function(product, productToImport) returing altered product based on ProductToImport data 
  * @returns {ok: boolean, err: string}
  */
-async function importSingleProduct(productToImport, token, alterProductBeforeImportCallback) {
+async function importSingleProduct(productToImport, token, alterProductBeforeImportCallback, index, count) {
   let { product, err } = await fetchCurrentProduct(productToImport.id, token);
   if (!err) {
     product = initialProductRecordSettings(product, productToImport);
-    product = alterProductBeforeImportCallback(product, productToImport);
+    product = alterProductBeforeImportCallback(product, productToImport, index, count);
     const result = await saveChangedProduct(product, token);
     return result;
   } else {
@@ -69,13 +71,13 @@ async function fetchCurrentProduct(id, token) {
     const resp = await axios.get(url, axconfig);
     return { product: resp.data, err: null }
   } catch (err) {
-      return { product: { }, err: null };  //new product
+    return { product: {}, err: null };  //new product
   };
 }
 
 function initialProductRecordSettings(prod, prodToImport) {
   if (!prod || Object.keys(prod).length === 0) {
-    prod = {"kind": "3e/product" };
+    prod = { "kind": "3e/product" };
     prod.pricelists = [
       {
         "name": "PL_default",
@@ -84,11 +86,11 @@ function initialProductRecordSettings(prod, prodToImport) {
     ];
     prod.__new = true;
   };
-  prod.id = prodToImport.id;      
-  
+  prod.id = prodToImport.id;
+
   if (!prod._) {
     prod._ = {};
-  }  
+  }
   return prod;
 }
 
@@ -101,10 +103,10 @@ async function saveChangedProduct(prod, token) {
   }
   let burl = `${process.env.BASE_URL}`;
   burl += burl.endsWith("/") ? "" : "/"
-  const url = `${burl}menu-api/api/items/${newProduct?'':prod.id}`;
+  const url = `${burl}menu-api/api/items/${newProduct ? '' : prod.id}`;
   const axcall = {
     url,
-    method: newProduct?'POST':'PUT',
+    method: newProduct ? 'POST' : 'PUT',
     headers: {
       'Accept': 'application/json',
       'Authorization': `Bearer ${token}`,
@@ -115,7 +117,7 @@ async function saveChangedProduct(prod, token) {
     const r = await axios.request(axcall);
     return { ok: true, err: null }
   } catch (err) {
-    return { ok: false, err:err.message }
+    return { ok: false, err: err.message }
   };
 }
 
