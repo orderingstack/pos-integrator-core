10008e95ec2a3292fb0e5abd6caa09cca0e4623d
Łukasz Niepala <lukasz.niepala@3e.pl>
2025-09-08 14:48:47 +0200

    refactor: move alert utilities to helpers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.6.2] - 2025-09-08
+
+- Refactor: move alert utilities to helpers module
+
 ## [2.6.0] - 2025-07-25
 
 - Feat: device code authorization support
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { getModuleConfig } from './authorization';
 import { logger } from './logger';
 
@@ -29,3 +30,48 @@ export const logError = (error: any) => {
     logger.error(`Error: ${error.message}`, error);
   }
 };
+
+export const AlertSeverity = {
+  DEBUG: 0,
+  INFO: 1,
+  WARN: 2,
+  ERROR: 3,
+  CRITICAL: 4,
+} as const;
+
+export type AlertSeverity = typeof AlertSeverity[keyof typeof AlertSeverity];
+
+export async function sendAlertMessage(
+  accessToken: string,
+  message: {
+    source: string;
+    eventName: string;
+    severity: AlertSeverity;
+    dateTime?: string;
+    user?: string;
+    tenant?: string;
+    orderId?: string;
+    venues?: string[];
+    params?: Record<string, string>;
+  },
+) {
+  if (!message.dateTime) message.dateTime = new Date().toISOString();
+  try {
+    const response = await axios.post(
+      `${process.env.BASE_URL}/alert-service/message`,
+      message,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    return { data: response.data };
+  } catch (error) {
+    return {
+      data: null,
+      error: error,
+    };
+  }
+}
